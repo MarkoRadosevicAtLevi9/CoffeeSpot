dee50c3c54c339752a2653a94e9d930983e0d630
MarkoRadosevic <mareee901@gmail.com>
2017-05-19 23:09:18 +0200

    first commit

---
+++ CoffeeSpot/App_Start/IoCConfigurator.cs
@@ -0,0 +1,33 @@
+﻿using System.Web.Http;
+using CoffeeSpot.Controllers;
+using CoffeeSpot.Data;
+using CoffeeSpot.Infrastructure;
+using CoffeeSpot.Repositories;
+using CoffeeSpot.Repositories.Interfaces;
+using Microsoft.Practices.Unity;
+using System.Web.Mvc;
+
+namespace CoffeeSpot.App_Start
+{
+    public static class IoCConfigurator
+    {
+        public static IUnityContainer ConfigureUnityContainer()
+        {
+            IUnityContainer container = new UnityContainer();
+
+            Register(container);
+
+            DependencyResolver.SetResolver(new UnityMvcDependencyResolver(container));
+
+            GlobalConfiguration.Configuration.DependencyResolver = new UnityWebApiDependencyResolver(container);
+
+            return container;
+        }
+
+        public static void Register(IUnityContainer container)
+        {
+            container.RegisterType<ICoffeeSpotRepository, CoffeeSpotRepository>();
+            container.RegisterType<ICoffeeSpotContext, CoffeeSpotContext>();
+        }
+    }
+}
---
+++ CoffeeSpot/Infrastructure/UnityMvcDependencyResolver.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Practices.Unity;
+using System;
+using System.Collections.Generic;
+using IDependencyResolver = System.Web.Mvc.IDependencyResolver;
+
+namespace CoffeeSpot.Infrastructure
+{
+    public class UnityMvcDependencyResolver : IDependencyResolver
+    {
+        private IUnityContainer container;
+
+        public UnityMvcDependencyResolver(IUnityContainer container)
+        {
+            this.container = container;
+        }
+
+        public object GetService(Type serviceType)
+        {
+            try
+            {
+                return container.Resolve(serviceType);
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            try
+            {
+                return container.ResolveAll(serviceType);
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+    }
+}
